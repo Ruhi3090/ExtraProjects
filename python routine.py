92dfab9032c419a651f88dbf31f328a5bc11cef8
Ruhi3090 <rm1204.kig@ked.edu.in>
2026-04-24 19:18:09 +0530

    Add files via upload

---
+++ python routine.py
@@ -0,0 +1,17 @@
+print("My Daily Routine")
+#Step 1:Waking up
+print("I wake up at 6:30 every weekday but on weekends I wake up at 12 o'clock")
+#Step 2:My morning routine
+print("Every school morning I:Freshen Up,Brush My Teeth,Bath,Dress Up and Have My Milk")
+#Step 3: Going To School
+print("I sit in my bus at 7:15 and reach at 8 o'clock")
+#step 4: After school
+print("I reach home at 4:45 then watch TV for 1 hour then go out to play")
+#Step 5:Studying
+print("I study for 2 hours")
+#Step 5: Rest of the stuff
+print("I have dinner and then sleep")
+#Step 6:Calculate total study time
+print("total time I study throughout the week is for",2*7,"hours")
+
+
